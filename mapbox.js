8c39226ed12b5e570cb6cb54fe21405c3d98b0b7
Dominic Simpson <db_simpson@yahoo.co.uk>
2023-10-26 12:54:13 +0100

    Initial commit

--- mapbox.js
+++ mapbox.js
@@ -27,3 +27,15 @@ map.on('draw.create', updateArea);
 map.on('draw.delete', updateArea);
 map.on('draw.update', updateArea);
 
+function updateArea(e) {
+  const data = draw.getAll();
+  const answer = document.getElementById('calculated-area');
+  if (data.features.length > 0) {
+    const area = turf.area(data);
+    const rounded_area = Math.round(area * 100) / 100;
+    answer.innerHTML = `<p><strong>${rounded_area}</strong></p><p>square meters</p>`;
+  } else {
+    answer.innerHTML = '';
+    if (e.type !== 'draw.delete') alert('Click the map to draw a polygon.');
+  }
+}
